bb596d78351becb8d93b8c2a87acee863ddfff16
Wimpzer <firestorm1992@hotmail.com>
2017-04-16 11:17:03 +0200

    Added SignalQualityActivity which will be worked on later when time is there

---
+++ app/src/main/java/com/musesleep/musesleep/SignalQualityActivity.java
@@ -0,0 +1,13 @@
+package com.musesleep.musesleep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignalQualityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signal_quality_activity);
+    }
+}
